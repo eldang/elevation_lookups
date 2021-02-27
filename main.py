dd04047275c8b2e7291a48ebe11f5af7421ea912
Eldan Goldenberg <eg@eldang.xyz>
2021-02-26 20:49:36 -0800

    Opens input file

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # Look up elevation data for a batch of paths
 
 import logging
+import os
 import time
 
 import click
@@ -13,15 +14,31 @@ __license__ = "Apache"
 
 @click.command()
 @click.option(
+    '--input_dir',
+    default='input',
+    help='Specify an input directory or leave out for default location.'
+)
+@click.option(
+    '--data_dir',
+    default='data',
+    help='Specify a data directory or leave out for default location.'
+)
+@click.option(
+    '--output_dir',
+    default='output',
+    help='Specify an output directory or leave out for default location.'
+)
+@click.option(
     '--log',
     type=click.Choice(
         ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
         case_sensitive=False
     ),
     default='INFO',
-    help='Logging level.  Only messages of the selected severity or higher will be emitted.'  # noqa: E501
+    help='Logging level.  Only messages of the selected severity or higher will be emitted.  Default: INFO'  # noqa: E501
 )
-def main(log):
+@click.argument('input_file')
+def main(input_dir, data_dir, output_dir, input_file, log):
     start_time = time.time()
     logging.basicConfig(
         format='%(asctime)s %(levelname)s:\t%(message)s',
@@ -29,6 +46,11 @@ def main(log):
         level=log
     )
     logging.debug("Starting run")
+    n_rows = 0
+    with open(os.path.join(input_dir, input_file)) as infile:
+        for row in infile:
+            n_rows += 1
+    logging.info("read %s rows", n_rows)
     logging.info("Run complete in %s.", elapsedTime(start_time))
 
 
