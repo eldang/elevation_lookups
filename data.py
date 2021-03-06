a7196061ae2a67d489d6d51f4a33c0f325195986
Eldan Goldenberg <eg@eldang.xyz>
2021-03-05 18:31:31 -0800

    made bbox into its own object type

---
+++ data.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# data source management
+
+
+
+class BoundingBox:
+    W: float
+    S: float
+    E: float
+    N: float
+    empty: bool = True
+
+
+    def __init__(
+        self,
+        W: float = None, S: float = None, E: float = None, N: float = None
+    ) -> None:
+        if W is not None:
+            self.W = W
+            self.empty = False
+        if S is not None:
+            self.S = S
+            self.empty = False
+        if E is not None:
+            self.E = E
+            self.empty = False
+        if N is not None:
+            self.N = N
+            self.empty = False
+
+
+    def __str__(self) -> str:
+        if self.empty:
+            return '[]'
+        else:
+            return 'W: ' + str(self.W) + ' S: ' + str(self.S) \
+                + ' E: ' + str(self.E) + ' N: ' + str(self.N)
--- files.py
+++ files.py
@@ -6,6 +6,7 @@ import logging
 import os
 from typing import Dict, List
 
+from data import BoundingBox
 
 
 class InputFile:
@@ -13,24 +14,24 @@ class InputFile:
     def __init__(self, input_dir: str, input_file: str) -> None:
         self.file_path: str = os.path.join(input_dir, input_file)
         self.n_rows: int = 0
-        self.bbox: Dict[str, float] = {}
+        self.bbox = BoundingBox()
 
         with open(self.file_path) as f:
             for row in f:
                 self.n_rows += 1
                 poly_line = PolyLine(row)
-                if self.bbox == {}:
+                if self.bbox.empty:
                     self.bbox = poly_line.getBbox()
                 else:
-                    bbox: Dict[str, float] = poly_line.getBbox()
-                    if bbox['W'] < self.bbox['W']:
-                        self.bbox['W'] = bbox['W']
-                    if bbox['S'] < self.bbox['S']:
-                        self.bbox['S'] = bbox['S']
-                    if bbox['E'] > self.bbox['E']:
-                        self.bbox['E'] = bbox['E']
-                    if bbox['N'] > self.bbox['N']:
-                        self.bbox['N'] = bbox['N']
+                    bbox: BoundingBox = poly_line.getBbox()
+                    if bbox.W is not None and bbox.W < self.bbox.W:
+                        self.bbox.W = bbox.W
+                    if bbox.S < self.bbox.S:
+                        self.bbox.S = bbox.S
+                    if bbox.E > self.bbox.E:
+                        self.bbox.E = bbox.E
+                    if bbox.N > self.bbox.N:
+                        self.bbox.N = bbox.N
         logging.info("Found %s rows in %s", self.n_rows, self.file_path)
         logging.info("Area covered: %s", self.bbox)
 
@@ -41,7 +42,7 @@ class PolyLine:
 
     def __init__(self, raw_row: str) -> None:
         self.coords: List[Dict[str, float]] = []
-        self.bbox: Dict[str, float] = {}
+        self.bbox = BoundingBox()
 
         for point in raw_row.split(" "):
             vals = point.split(",")
@@ -50,21 +51,22 @@ class PolyLine:
                 'y': float(vals[1])
             })
 
-    def getBbox(self) -> Dict[str, float]:
-        if self.bbox == {}:
-            self.bbox = {
-                'W': self.coords[0]['x'],
-                'S': self.coords[0]['y'],
-                'E': self.coords[0]['x'],
-                'N': self.coords[0]['y']
-            }
+    def getBbox(self) -> BoundingBox:
+        if self.bbox.empty:
+            self.bbox = BoundingBox(
+                self.coords[0]['x'],
+                self.coords[0]['y'],
+                self.coords[0]['x'],
+                self.coords[0]['y']
+            )
+
             for coord in self.coords[1:]:
-                if coord['x'] < self.bbox['W']:
-                    self.bbox['W'] = coord['x']
-                if coord['y'] < self.bbox['S']:
-                    self.bbox['S'] = coord['y']
-                if coord['x'] > self.bbox['E']:
-                    self.bbox['E'] = coord['x']
-                if coord['y'] > self.bbox['N']:
-                    self.bbox['N'] = coord['y']
+                if coord['x'] < self.bbox.W:
+                    self.bbox.W = coord['x']
+                if coord['y'] < self.bbox.S:
+                    self.bbox.S = coord['y']
+                if coord['x'] > self.bbox.E:
+                    self.bbox.E = coord['x']
+                if coord['y'] > self.bbox.N:
+                    self.bbox.N = coord['y']
         return self.bbox
