98feb3e1f605f4f653cca7ead5263b68d9bae37d
Eldan Goldenberg <eg@eldang.xyz>
2021-03-08 17:03:51 -0800

    clearer methods for accessing info in an InputFile

--- files.py
+++ files.py
@@ -12,16 +12,14 @@ class InputFile:
 
     def __init__(self, input_dir: str, input_file: str) -> None:
         self.file_path: str = os.path.join(input_dir, input_file)
-        self.n_rows: int = 0
 
         lines: List[LineString] = []
         with open(self.file_path) as f:
             for row in f:
-                self.n_rows += 1
                 lines.append(self.__buildLine__(row))
-        self.paths = MultiLineString(lines)
-        logging.info("Found %s rows in %s", self.n_rows, self.file_path)
-        logging.info("Area covered: %s", self.paths.bounds)
+        self.__paths = MultiLineString(lines)
+        logging.info("Found %s rows in %s", self.n_lines(), self.file_path)
+        logging.info("Area covered: %s", self.bbox())
 
     def __buildLine__(self, raw_line) -> LineString:
         coords: List[Tuple[float, float]] = []
@@ -29,3 +27,9 @@ class InputFile:
             vals = [float(x) for x in point.split(",")]
             coords.append((vals[0], vals[1]))
         return LineString(coords)
+
+    def bbox(self) -> Tuple[float, float, float, float]:
+        return self.__paths.bounds
+
+    def n_lines(self) -> int:
+        return len(self.__paths.geoms)
